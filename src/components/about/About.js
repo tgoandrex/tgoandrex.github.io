6d16ba7b8a781816be8d3b9c90e87d319446bc3f
tgoandrex <tgoandrex@gmail.com>
2023-03-04 18:30:27 -0600

    Replace animated name with animated 3D cube.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,8 +2,15 @@ function About() {
     return (
         <section id='about'>
             <div className='flex'>
-                <div id='about-animation'>
-                    <span id='animation-text'>Andrew Stapleton</span>
+                <div id='cube-container'>
+                    <div class="cube">
+                        <div class="side front"></div>
+                        <div class="side back"></div>
+                        <div class="side left"></div>
+                        <div class="side right"></div>
+                        <div class="side top"></div>
+                        <div class="side bottom"></div>
+                    </div>
                 </div>
                 <div id='about-summary'>
                     Summary
