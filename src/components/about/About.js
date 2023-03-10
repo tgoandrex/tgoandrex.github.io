e95ad1356e9167182d714d4d44db0af3af570c60
tgoandrex <tgoandrex@gmail.com>
2023-03-10 01:26:50 -0600

    More about section css restructuring.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -13,7 +13,25 @@ function About() {
                     </div>
                 </div>
                 <div id='about-summary'>
-                    Summary
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                        deserunt mollit anim id est laborum.
+                    </p>
+                    <p>
+                        Sagittis eu volutpat odio facilisis mauris sit amet. Posuere ac ut consequat semper viverra nam. 
+                        Libero justo laoreet sit amet cursus. Libero id faucibus nisl tincidunt eget nullam non. Fringilla 
+                        urna porttitor rhoncus dolor purus non enim praesent. Amet est placerat in egestas erat imperdiet sed. 
+                        Ut placerat orci nulla pellentesque. Habitant morbi tristique senectus et netus et. Vel facilisis 
+                        volutpat est velit egestas dui id ornare. Adipiscing elit pellentesque habitant morbi tristique senectus. 
+                        Maecenas pharetra convallis posuere morbi leo. Sit amet volutpat consequat mauris nunc congue nisi 
+                        vitae suscipit. Est lorem ipsum dolor sit amet. Montes nascetur ridiculus mus mauris vitae ultricies leo. 
+                        Iaculis at erat pellentesque adipiscing commodo. Volutpat maecenas volutpat blandit aliquam etiam 
+                        erat velit. Nulla malesuada pellentesque elit eget gravida cum sociis. Aliquet risus feugiat in ante 
+                        metus dictum. Diam maecenas sed enim ut sem viverra aliquet eget sit.
+                    </p>
                 </div>
             </div>
         </section>
