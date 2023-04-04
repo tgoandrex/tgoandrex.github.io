e6bf4af2a701d2231cde87fd47134652e3f77dca
tgoandrex <tgoandrex@gmail.com>
2023-04-03 22:49:00 -0500

    Further contact form CSS adjustments. Install EmailJS to allow submitted form to send content to my personal email address.

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,32 +1,48 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
     const [showContactForm, setShowContactForm] = useState(false);
+
+    const form = useRef();
 
     const toggleClick = () => {
         setShowContactForm(!showContactForm);
     }
 
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_i5y0t9o', 'template_fnl7odc', form.current, 'S2iuW3trdarfcK_1v')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
     return (
         <section id='contact'>
             <button className='contact-btn' onClick={()=> {toggleClick()}}>Let's Get in Touch</button>
             {showContactForm &&
-                <form className='contact-form'>
-                    <p>* Required Field</p>
-                    <div className='form-section flex'>
-                        <label htmlFor='form-name'>Name or Business *</label>
-                        <input type='text' id='form-name' required />
+                <form className='contact-form' ref={form} onSubmit={sendEmail}>
+                    <div className='form-section'>
+                        * Required Field
                     </div>
                     <div className='form-section flex'>
-                        <label htmlFor='form-email'>Email</label>
-                        <input type='email' id='form-email'/>
+                        <label htmlFor='user_name'>Name *</label>
+                        <input type='text' id='user_name' name='user_name' required />
                     </div>
                     <div className='form-section flex'>
-                        <label htmlFor='form-message'>Message *</label>
-                        <textarea id='form-message' required />
+                        <label htmlFor='user_email'>Email</label>
+                        <input type='email' id='user_email' name='user_email'/>
+                    </div>
+                    <div className='form-section flex'>
+                        <label htmlFor='message'>Message *</label>
+                        <textarea id='message' name='message' required />
                     </div>
                     <div className='form-section'>
-                        <button type='submit'>Submit</button>
+                        <button className='submit-btn' type='submit'>Submit</button>
                     </div>
                 </form>
             }
