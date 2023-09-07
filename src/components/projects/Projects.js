b6b31b85d682418c03213e0dde49ff145a3d8559
tgoandrex <tgoandrex@gmail.com>
2023-09-07 17:20:31 -0500

    Replace Memory Game project image with updated image. Change Memory Game description slightly

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -14,7 +14,7 @@ function Projects() {
         description: `Find Waldo in the picture and get the high score! The less time you took to find Waldo the higher your score will be! (I claim no legal ownership to 'Where's Waldo?'. This application was strictly made for educational purposes only.) (Please allow a moment for the scoreboards to display, as the project's backend is deployed under render.com's 'free' plan)`,
         link: 'https://wheres-waldo-mxpj.onrender.com/', src: waldo},
         {title: 'Memory Game',
-        description: `Choose and remember which Pokemon you chose to get the high score! Three levels of difficulty for a maximum of nine Pokemon! (I claim no legal ownership to 'Pokemon'. This application was strictly made for educational purposes only.)`,
+        description: `Choose and remember which Pokemon you chose to get the high score! A maximum of nine Pokemon by level 3! (I claim no legal ownership to 'Pokemon'. This application was strictly made for educational purposes only.)`,
         link: 'https://idyllic-cranachan-473ef6.netlify.app/', src: memory},
         {title: 'Nodejs Mini Message Board',
         description: `Add a message to the message board for others to read! You have the ability to edit and delete your own messages too! Features user authentication in order to post messages! (Please allow a moment for the app to display, as the project's backend is deployed under render.com's 'free' plan)`,
