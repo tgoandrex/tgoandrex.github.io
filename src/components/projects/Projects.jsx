9377562c7b1acd66feed52fa4633f52e4a16df50
tgoandrex <tgoandrex@gmail.com>
2024-11-08 20:34:07 -0600

    Change mobile view for projects page to display project items instead of accordion. Add accordion.css and move accordion related css to that.

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Accordion from './Accordion';
 
 import './projects.css';
+import './accordion.css';
 
 import waldo from '../../images/wheres-waldo.PNG';
 import spacebook from '../../images/spacebook.PNG';
@@ -11,7 +12,24 @@ import messageBoard from '../../images/mini-message-board.PNG';
 import clown from '../../images/clown-landing.PNG';
 
 function Projects() {
-    const [activeImage, setActiveImage] = useState(null);
+    const [toggleMobileView, setToggleMobileView] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+          if(window.innerWidth > 600) {
+            setToggleMobileView(false);
+          } else {
+            setToggleMobileView(true);
+          }
+        }
+
+        handleResize();
+    
+        window.addEventListener("resize", handleResize);
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        }
+      }, []);
 
     const projects = [
         {title: 'Spacebook', description: `A social media application with a sitewide light and dark (space) theme. 
@@ -34,22 +52,65 @@ function Projects() {
         link: 'https://nodejs-mini-message-board.onrender.com/', src: messageBoard}
     ]
 
-    const handleAccordionClick = (index) => {
-        setActiveImage(projects[index].src);
-    };
+    const ProjectsDesktop = () => {
+        const [activeImage, setActiveImage] = useState(null);
+        
+        const handleAccordionClick = (index) => {
+            setActiveImage(projects[index].src);
+        };
+
+        return (
+            <>
+                <div className='accordion'>
+                    <Accordion projects={projects} onAccordionClick={handleAccordionClick} />
+                </div>
+                <div className='projects-image-container'>
+                    {activeImage && <img src={activeImage} alt='Project' />}
+                </div>
+            </>
+        )
+    }
+
+    const ProjectsMobile = () => {
+        const [expandedProjectIndex, setExpandedProjectIndex] = useState(null);
+
+        const handleProjectClick = (index) => {
+            setExpandedProjectIndex(prevIndex => prevIndex === index ? null : index);
+        };
+
+        return (
+            projects.map((project, index) => {
+                const isExpanded = expandedProjectIndex === index;
+
+                return (
+                    <div className='projects-mobile-item' key={index} onClick={() => handleProjectClick(index)}>
+                        <img src={project.src} alt='Project' />
+                        <div className='projects-mobile-description'>
+                            {isExpanded ? (
+                                <>
+                                    {project.description}
+                                    <br />
+                                    <a href={project.link}>Live Demo</a>
+                                </>
+                            ) : (
+                                <span>Click anywhere to expand description</span>
+                            )}
+                        </div>
+                    </div>
+                )
+            })
+        )
+    }
 
     return (
         <section id='projects'>
             <div className='projects-container'>
                 <h3 className='projects-header'>Projects</h3>
-                <div className='projects-subcontainer'>
-                    <div className='accordion'>
-                        <Accordion projects={projects} onAccordionClick={handleAccordionClick} />
-                    </div>
-                    <div className='projects-image-container'>
-                        {activeImage && <img src={activeImage} alt="Project" />}
-                    </div>
-                </div>
+                {!toggleMobileView ?
+                    <ProjectsDesktop />
+                :
+                    <ProjectsMobile />
+                }
             </div>
         </section>
     )
