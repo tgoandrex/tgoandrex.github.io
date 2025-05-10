2cdb4c4d6ce5163eb11cee061d24273acd1c040b
tgoandrex <tgoandrex@gmail.com>
2025-05-09 23:34:45 -0500

    Change Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -24,14 +24,21 @@ const Home = () => {
           animate={{ opacity: 1 }}
           transition={{ delay: 3.5 }}
         >
-          I'm a full stack web developer.
+          I'm a self taught web developer.
         </motion.p>
         <motion.p
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 4.5 }}
         >
-          I like to work with JavaScript, React, Node, PostgreSQL or MongoDB, and Express
+          I like to work with JavaScript, React, Python, HTML, and CSS.
+        </motion.p>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 5.5 }}
+        >
+          Everything you see here (including this prortfolio) was made by me.
         </motion.p>
       </motion.div>
     </section>
